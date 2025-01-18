bfa7f7cfd8fab2f336c301016656accb1291272b
Hemang Rathod «⟨мя.ꍟꀸ⟩» <101561859+mred0@users.noreply.github.com>
2025-01-18 20:16:22 +0530

    catalogue_id issue in post man

--- src/controllers/catalogueController.js
+++ src/controllers/catalogueController.js
@@ -19,35 +19,23 @@ exports.getCatalogues = async (req, res) => {
 
 exports.postCatalogue = async (req, res) => {
   try {
-    const {
-      catalogue_id,
-      catalogue_name,
-      catalogue_image_url,
-      catalogue_pdf_link,
-    } = req.body;
+    const { catalogue_name, catalogue_image_url, catalogue_pdf_link } =
+      req.body;
 
-    if (
-      !catalogue_id ||
-      !catalogue_name ||
-      !catalogue_image_url ||
-      !catalogue_pdf_link
-    ) {
+    if (!catalogue_name || !catalogue_image_url || !catalogue_pdf_link) {
       return res.status(400).json({ error: "All fields are required." });
     }
 
     const catalogue = await Catalogue.create({
-      catalogue_id,
       catalogue_name,
       catalogue_image_url,
       catalogue_pdf_link,
     });
 
-    res
-      .status(201)
-      .json({
-        message: "Catalogue Created Successfully.",
+    res.status(201).json({
+      message: "Catalogue Created Successfully.",
         catalogue_id: catalogue_id,
-      });
+    });
   } catch (err) {
     res
       .status(500)
--- src/models/Catalogue.js
+++ src/models/Catalogue.js
@@ -1,23 +1,28 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../config/db");
 
-const Catalogue = sequelize.define("Catalogue", {
-  catalogue_id: {
-    type: DataTypes.INTEGER,
-    unique: true,
-    allowNull: false,
+const Catalogue = sequelize.define(
+  "Catalogue",
+  {
+    catalogue_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true, // Ensures auto-generation
+      allowNull: false,
+    },
+    catalogue_name: {
+      type: DataTypes.STRING,
+    },
+    catalogue_image_url: {
+      type: DataTypes.STRING,
+    },
+    catalogue_pdf_link: {
+      type: DataTypes.STRING,
+    },
   },
-  catalogue_name: {
-    type: DataTypes.STRING,
-  },
-  catalogue_image_url: {
-    type: DataTypes.STRING
-  },
-  catalogue_pdf_link:{
-    type: DataTypes.STRING
+  {
+    timestamps: false,
   }
-}
 );
-
 
 module.exports = Catalogue;
